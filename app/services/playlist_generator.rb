b8f250e0984d7270c800fa8a222a6f10ec59b31d
Philipp Rosauer <35028581+prosauer@users.noreply.github.com>
2026-01-26 16:18:43 +0100

    Regenerate missing Tidal playlists

--- app/services/playlist_generator.rb
+++ app/services/playlist_generator.rb
@@ -5,20 +5,25 @@ class PlaylistGenerator
 
   def generate(name:, tracks:, week: nil)
     existing_playlist = UserPlaylist.find_by(user: @user, name: name)
+    tidal_account = @user.tidal_account
+    tidal_service = TidalService.new
+    access_token = ensure_tidal_access_token(tidal_service, tidal_account) if tidal_account
+
     if existing_playlist
-      if week && week.tidal_playlist_url.blank?
-        week.update(tidal_playlist_url: existing_playlist.tidal_url)
+      playlist_id = extract_playlist_id(existing_playlist.tidal_url)
+      if access_token.present? && playlist_id.present? && tidal_service.playlist_exists?(playlist_id, access_token: access_token)
+        if week && week.tidal_playlist_url.blank?
+          week.update(tidal_playlist_url: existing_playlist.tidal_url)
+        end
+        return existing_playlist.tidal_url
       end
-      return existing_playlist.tidal_url
+
+      return existing_playlist.tidal_url if access_token.blank?
+
+      existing_playlist.destroy!
     end
 
     return if tracks.empty?
-
-    tidal_account = @user.tidal_account
-    return unless tidal_account
-
-    tidal_service = TidalService.new
-    access_token = ensure_tidal_access_token(tidal_service, tidal_account)
     return unless access_token
 
     tidal_url = tidal_service.create_playlist(
@@ -35,6 +40,11 @@ class PlaylistGenerator
   end
 
   private
+
+  def extract_playlist_id(url)
+    match = url.to_s.match(%r{playlist/([a-zA-Z0-9\-]+)})
+    match ? match[1] : nil
+  end
 
   def ensure_tidal_access_token(service, tidal_account)
     return tidal_account.access_token unless tidal_account.expired? && tidal_account.refresh_token.present?
